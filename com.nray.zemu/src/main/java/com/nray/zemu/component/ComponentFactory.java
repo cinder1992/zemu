f97ab96133eb3ec06d208c3802d1307e9213ddd1
nray <nray@samwise>
2018-07-21 19:07:43 +0100

    working commit

--- com.nray.zemu/src/main/java/com/nray/zemu/component/ComponentFactory.java
+++ com.nray.zemu/src/main/java/com/nray/zemu/component/ComponentFactory.java
@@ -3,10 +3,19 @@ import org.json.*;
 
 public final class ComponentFactory {
 	public static Component createComponent(JSONObject Component) {
-		JSONObject ComponentData = Component.getJSONObject("ComponentData");
+		JSONObject ComponentData;
+		try {
+			ComponentData = Component.getJSONObject("ComponentData");
+		}
+		catch (JSONException e) {
+			throw e;
+		}
 		switch (Component.getString("Type").toUpperCase()) {
 			case "RAM":
 				return new Ram(ComponentData);
+				
+			case "ROM":
+				return new Rom(ComponentData);
 
 			default:
 				return null;
---
+++ com.nray.zemu/src/main/java/com/nray/zemu/component/Rom.java
@@ -0,0 +1,22 @@
+package com.nray.zemu.component;
+import org.json.*;
+
+public class Rom implements Component {
+	
+	public Rom(JSONObject ComponentData) {
+		
+	}
+
+	@Override
+	public void write(int val, int address) throws ArrayIndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int read(int address) throws ArrayIndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
